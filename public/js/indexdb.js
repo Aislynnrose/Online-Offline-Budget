7e68ae9466af2773ce0768be6d1248fa7bdd4a76
Aislynn Uribe <aislynn.rose@gmail.com>
2021-10-22 17:59:50 -0600

    trying to resolve TypeError

--- public/js/indexdb.js
+++ public/js/indexdb.js
@@ -5,59 +5,60 @@ const indexedDB =
   window.msIndexedDB || 
   window.shimIndexedDB ;
 
-let data;
-const req = indexedDB.open("budget", 1);
+let db;
+const request = indexedDB.open("budget", 1);
 
-req.onupgradeneeded = ({ target }) => {
-  let data = target.result;
-  data.createObjectStore("pending", { autoIncrement: true });
+request.onupgradeneeded = ({ target }) => {
+  let db = target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
 };
 
-req.onsuccess = ({ target }) => {
-  let data = target.result;
+request.onsuccess = () => {
+  // let db = target.result;
   if (navigator.onLine) {
-    checkDb();
+    checkDatabase();
   }
 };
 
-req.onerror = ({ target }) => {
+request.onerror = ({ target }) => {
   console.log(target.errorCode);
 };
 
-function saveRecord(data) {
-  const trans = data.transaction(["pending"], "readwrite");
-  const save = trans.objectStore("pending");
-  save.add(data);
-}
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  store.add(record);
+};
 
-function checkDb() {
-  const trans = data.transaction(["pending"], "readwrite");
-  const save = trans.objectStore("pending");
-  const getAllData = save.getAll();
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
 
-  getAllData.onsuccess = () => {
-    console.log('working');
-    // if (getAllData.result.length > 0) {
-    //   fetch("/api/transaction/bulk", {
-    //     method: "POST",
-    //     body: JSON.stringify(getAllData.result),
-    //     headers: {
-    //       Accept: "application/json, text/plain, */*",
-    //       "Content-Type": "application/json"
-    //     },
-    //   })
-    //     .then((response) => {
-    //       return response.json();
-    //     })
-    //     .then(() => {
-    //       const transaction = data.transaction(["pending"], "readwrite");
-    //       const save = transaction.objectStore("pending");
-    //       save.clear();
-    //     });
-    // }
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function(){
+    
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        },
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then(() => {
+          const transaction = db.transaction(["pending"], "readwrite");
+          const save = transaction.objectStore("pending");
+          save.clear();
+        });
+    }
     // // create if statement that checks if the legnth of the getAllData.result >0 then fetch the api/transaction/bulk method post then json the response
     //then delete the record if it successully saves the records in the DB. by running clear()
   };
 }
 
-window.addEventListener("online", checkDb);
+window.addEventListener("online", checkDatabase);
